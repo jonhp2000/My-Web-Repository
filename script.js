bd82e0752952eb652ff88404af582f7fcc6c0de2
jonhp2000 <jonhp2000@gmail.com>
2021-09-12 19:45:35 +0100

    Start project with a test

---
+++ script.js
@@ -0,0 +1,19 @@
+const messages = [
+  [
+    "Não sabendo que era impossível foi lá e fez.",
+    "O segredo de progredir é começar",
+    "A persistência é o caminho do êxito",
+  ],
+  [
+    "Uma curva na estrada não é o fim do caminho, a não ser que você decida não virar",
+    "Corra, viva, sonhe e alcance!",
+    "Um dia que começa agradecido termina realizado.",
+  ],
+  [
+    "Quando você deseja o bem, o bem te deseja também.",
+    "Insista, persista e nunca desista",
+    "Decida onde quer chegar e não pare até conseguir",
+  ],
+];
+
+console.log(messages[0][1] + "\n" + messages[1][1]);
